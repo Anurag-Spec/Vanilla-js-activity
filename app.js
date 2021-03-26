49a8ea906bda7ed45fc22bbe90fce3c61344862c
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-03-27 01:27:27 +0530

    input output done

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
 btnTranslate.addEventListener("click", function clickEventHandler(){
-    console.log("clicked");
-    console.log("input", txtInput.value);
+
+    outputDiv.innerText = "asas " + txtInput.value
+  
 })
 
 
