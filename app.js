75f34eac35853ab4985c1311b026d116fa2d6c86
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-03-27 00:50:13 +0530

    taking input and console

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
 
 btnTranslate.addEventListener("click", function clickEventHandler(){
-    
+    console.log("clicked");
+    console.log("input", txtInput.value);
 })
 
 
