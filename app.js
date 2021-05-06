369eb1812932b866c1da2c7afa90c35f8f34aa7e
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-05-06 11:46:27 +0530

    main-changes

--- app.js
+++ app.js
@@ -2,26 +2,26 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-var serverUrl =  "https://api.funtranslations.com/translate/minion.json";
+var serverUrl =  "https://api.funtranslations.com/translate/mandalorian.json";
 
 function getTranslateUrl (text) {
  return serverUrl + "?" + "text=" + text
 }
 
-btnTranslate.addEventListener("click", function clickEventHandler(){
-
-    // outputDiv.innerText = "aji " + txtInput.value
-  
-var inputTxt = txtInput.value;
-
 function errorHandler(error){
     console.log("error occured".error)
+    alert("Something wrong with server please try after some time")
 }
+
+ function clickHandler(){ 
+var inputTxt = txtInput.value;
 
 fetch(getTranslateUrl(inputTxt))
 .then(response =>response.json())
-.then(json => console.log(json.contents.translated))
-.catch(errorHandler);
+.then(json => {
+    outputDiv.innerText = json.contents.translated;
 })
+.catch(errorHandler);
+ }
 
-
+btnTranslate.addEventListener("click", clickHandler);
