23566fe7ee32dfd76869cd643aaebcac8b418965
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-05-06 10:48:50 +0530

    error-handler

--- app.js
+++ app.js
@@ -2,10 +2,26 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+var serverUrl =  "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslateUrl (text) {
+ return serverUrl + "?" + "text=" + text
+}
+
 btnTranslate.addEventListener("click", function clickEventHandler(){
 
-    outputDiv.innerText = "asas " + txtInput.value
+    // outputDiv.innerText = "aji " + txtInput.value
   
+var inputTxt = txtInput.value;
+
+function errorHandler(error){
+    console.log("error occured".error)
+}
+
+fetch(getTranslateUrl(inputTxt))
+.then(response =>response.json())
+.then(json => console.log(json.contents.translated))
+.catch(errorHandler);
 })
 
 
